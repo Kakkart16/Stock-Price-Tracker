91eae836128e2618c7640b7f856ab47b60f10bae
Kakkart16 <kakkartushar.16@gmail.com>
2024-02-23 15:36:38 +0530

    Added multi threading process

--- stocks/tracker/views.py
+++ stocks/tracker/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 import yahoo_fin.stock_info as si
 # from yahoo_fin import stock_info
 import yfinance as yf
+import time
+import queue
+from threading import Thread
+import json
+
 
 def home(request):
     stock_picker = si.tickers_nifty50()
@@ -22,9 +27,25 @@ def home(request):
 
 def stockTracker(request):
     selected_stocks = request.GET.getlist('stock')    
-    data = []
-    for stock in selected_stocks:
-        quote = yf.Ticker(stock).info
+    dataX = {}
+    # for stock in selected_stocks:
+    #     quote = yf.Ticker(stock).info
+    #     # print(quote)
+    #     details = {
+    #         'name': quote['shortName'],
+    #         'price': quote["currentPrice"],
+    #         'prev_close': quote["previousClose"], 
+    #         'open': quote["open"],
+    #         'market_cap': quote["marketCap"],
+    #         'volume': quote["volume"],          
+    #     }
+    #     data.append(details)
+    # print(data)
+    n_threads = len(selected_stocks)
+    thread_list = []
+    que = queue.Queue()
+    for i in range(n_threads):
+        quote = yf.Ticker(selected_stocks[i]).info
         # print(quote)
         details = {
             'name': quote['shortName'],
@@ -34,6 +55,20 @@ def stockTracker(request):
             'market_cap': quote["marketCap"],
             'volume': quote["volume"],          
         }
-        data.append(details)
+        thread = Thread(target = lambda q, arg1: q.put({selected_stocks[i]: details}), args = (que, selected_stocks[i]))
+        thread_list.append(thread)
+        thread_list[i].start()
+
+    for thread in thread_list:
+        thread.join()
+
+    while not que.empty():
+        result = que.get()
+        dataX.update(result)
+        
+    # print(dataX)
+    data = [value for value in dataX.values()]
+
     # print(data)
+    
     return render(request, 'stockTracker.html', {'data': data})
